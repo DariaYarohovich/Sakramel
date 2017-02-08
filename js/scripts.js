ffdd4db8aca497d9321dceff4d0a77537ad1f892
mozolevsky <alf.rulit@gmail.com>
2017-02-09 01:52:23 +0300

    Bugfixing: tablet version

--- js/scripts.js
+++ js/scripts.js
@@ -9,4 +9,19 @@ menuOpen.addEventListener('click', function () {
     menuOpenIcon.classList.toggle('header__open-icon_active');
 })
 
+ if (document.body.clientWidth >= 768) {
+        var slider = document.querySelector('.faq > .swiper-container');
+        var swiperWrapper = slider.querySelector('.swiper-wrapper');
+        
+        slider.classList.remove('swiper-container');
+        swiperWrapper.classList.remove('swiper-wrapper');
+        swiperWrapper.classList.add('review__desctop-slider');
+
+        for (var i = 0; i < swiperWrapper.children.length; i ++) {
+            swiperWrapper.children[i].className = '';
+            swiperWrapper.children[i].style = null;
+
+        }
+    }
+
 
